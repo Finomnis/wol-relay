179d6a98a7d9022a8a09bfd200accbbfb49453d1
Finomnis <finomnis@gmail.com>
2025-08-02 14:07:41 +0200

    Implement the acutal forwarding

--- src/bin/wol-relay.rs
+++ src/bin/wol-relay.rs
@@ -5,6 +5,11 @@ fn main() {
     env_logger::Builder::from_env(Env::default().default_filter_or("debug")).init();
 
     for target_mac in WolReceiver::new().run().unwrap() {
-        log::info!("Received WoL: {target_mac}");
+        log::info!("Relaying WoL packet for {target_mac}");
+        if let Err(e) =
+            wol::send_magic_packet(target_mac, secure_on, (Ipv4Addr::BROADCAST, 9).into())
+        {
+            log::error!("Failed to send WoL packet: {}", e);
+        }
     }
 }
